332e01368ab45beaa0a62caebf539007a41adb38
Lakshya Vats <146511679+p1x3lph4nt0m@users.noreply.github.com>
2025-02-25 04:51:54 +0530

    Update lab6.c

--- lab6.c
+++ lab6.c
@@ -2,15 +2,68 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
-int row=0;
-void *prod(void*vargp){
-	for(int i=0;i<n;i++){
-		for(int j=0;j<n;j++){
-		matC[row][i]+=matA[row][k]*mat[k][i];
-		}
+typedef struct{
+	int** a1;
+	int** a2;
+	int** a3;
+	int n1;
+	int n2;
+}MatrixData;
+int n,m,o;
+void *prod(void* vargp){
+	MatrixData* data =(MatrixData*) vargp;
+	int x=0;
+	for(int i=0;i<m;i++){
+		x+=(data->a1)[data->n1][i]*(data->a2)[i][data->n2];
 	}
-	row++;
+	data->a3[data->n1][data->n2]=x;
+	free(data);
+	pthread_exit(NULL);
 }
 int main(){
+	scanf("%d %d %d",&n,&m,&o);
+	int** matA=(int**)malloc(n*sizeof(int*));
+	int** matB=(int**)malloc(m*sizeof(int*));
+	int** matC=(int**)malloc(n*sizeof(int*));
+	for(int i=0;i<n;i++)matA[i]=(int*)malloc(m*sizeof(int));
+	for(int i=0;i<m;i++)matB[i]=(int*)malloc(o*sizeof(int));
+	for(int i=0;i<n;i++)matC[i]=(int*)malloc(o*sizeof(int));
+	for(int i=0;i<n;i++){
+		for(int j=0;j<m;j++){
+			scanf("%d",&matA[i][j]);
+		}
+	}
+	for(int i=0;i<m;i++){
+		for(int j=0;j<o;j++){
+			scanf("%d",&matB[i][j]);
+		}
+	}
+	pthread_t th[n][o];
+	for(int i=0;i<n;i++){
+		for(int j=0;j<o;j++){
+			MatrixData *data=(MatrixData *)malloc(sizeof(MatrixData));
+			data->n1=i;data->n2=j;
+			data->a1=matA;data->a2=matB;data->a3=matC;
+			pthread_create(&th[i][j],NULL,prod,data);
+		}
+	}
+	for(int i=0;i<n;i++){
+		for(int j=0;j<o;j++){
+			pthread_join(th[i][j],NULL);
+		}
+	}
+	for(int i=0;i<n;i++){
+		for(int j=0;j<o;j++){
+			printf("%d ",matC[i][j]);
+		}
+		printf("\n");
+	}
+	for(int i=0;i<n;i++)free(matA[i]);
+	for(int i=0;i<m;i++)free(matB[i]);
+	for(int i=0;i<n;i++)free(matC[i]);
+	free(matA);free(matB);free(matC);
+	return 0;
+}
+
 
 	
